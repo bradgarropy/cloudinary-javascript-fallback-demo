e681586a27fbae0a1b519448244e38a6982d74c8
Brad Garropy <bradgarropy@gmail.com>
2024-08-02 09:52:20 -0500

    done.

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,49 @@
 import "~/src/style.css"
 
-import viteLogo from "/vite.svg"
-import {setupCounter} from "~/src/counter"
-import typescriptLogo from "~/src/typescript.svg"
+import {Cloudinary} from "@cloudinary/url-gen"
+import {defaultImage} from "@cloudinary/url-gen/actions/delivery"
+
+const cloudinary = new Cloudinary({
+    cloud: {
+        cloudName: "bradgarropy",
+    },
+    url: {
+        analytics: false,
+    },
+})
+
+const a = cloudinary
+    .image("cloudinary-javascript-fallback-demo/a")
+    .format("auto")
+    .quality("auto")
+    .toURL()
+
+const b = cloudinary
+    .image("cloudinary-javascript-fallback-demo/b")
+    .format("auto")
+    .quality("auto")
+    .toURL()
+
+// STEP 1
+// const c = cloudinary
+//     .image("cloudinary-javascript-fallback-demo/c")
+//     .format("auto")
+//     .quality("auto")
+//     .delivery(defaultImage("cloudinary-javascript-fallback-demo:sea.jpg"))
+//     .toURL()
+
+// STEP 2
+const cBroken = cloudinary
+    .image("cloudinary-javascript-fallback-demo/cBroken")
+    .format("auto")
+    .quality("auto")
+    .delivery(defaultImage("cloudinary-javascript-fallback-demo:sea.jpg"))
+    .toURL()
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-
-    <h1>Vite + TypeScript</h1>
-
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <div class="letters">
+      <img src="${a}" />
+      <img src="${b}" />
+      <img src="${cBroken}" />
     </div>
-
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!)
